597a90f26602d4c6db87ab5709d818c3b8200ba1
AO Industries, Inc. <support@aoindustries.com>
2017-08-09 11:02:21 -0500

    Removed use of File.deleteOnExit() to avoid memory leaks in long-running applications.

--- src/main/java/com/semanticcms/dia/taglib/DiaTag.java
+++ src/main/java/com/semanticcms/dia/taglib/DiaTag.java
@@ -23,11 +23,8 @@
 package com.semanticcms.dia.taglib;
 
 import static com.aoindustries.encoding.Coercion.zeroIfEmpty;
-import com.aoindustries.io.TempFileList;
-import com.aoindustries.io.buffer.AutoTempFileWriter;
 import com.aoindustries.io.buffer.BufferResult;
 import com.aoindustries.io.buffer.BufferWriter;
-import com.aoindustries.servlet.filter.TempFileContext;
 import static com.aoindustries.taglib.AttributeUtils.resolveValue;
 import com.aoindustries.taglib.AutoEncodingBufferedTag;
 import com.semanticcms.core.model.ElementContext;
@@ -106,18 +103,7 @@ public class DiaTag extends ElementTag<Dia> {
 			final HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
 			BufferWriter capturedOut;
 			if(captureLevel == CaptureLevel.BODY) {
-				// Enable temp files if temp file context active
-				capturedOut = TempFileContext.wrapTempFileList(
-					AutoEncodingBufferedTag.newBufferWriter(),
-					request,
-					// Java 1.8: AutoTempFileWriter::new
-					new TempFileContext.Wrapper<BufferWriter>() {
-						@Override
-						public BufferWriter call(BufferWriter original, TempFileList tempFileList) {
-							return new AutoTempFileWriter(original, tempFileList);
-						}
-					}
-				);
+				capturedOut = AutoEncodingBufferedTag.newBufferWriter(request);
 			} else {
 				capturedOut = null;
 			}
