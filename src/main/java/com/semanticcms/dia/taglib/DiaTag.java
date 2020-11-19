32de37b760c004b1944ca202be59a9539f83ff17
AO Industries, Inc <support@aoindustries.com>
2020-11-19 02:48:08 -0600

    New project "ao-encoding-taglib" split from "ao-taglib"

--- src/main/java/com/semanticcms/dia/taglib/DiaTag.java
+++ src/main/java/com/semanticcms/dia/taglib/DiaTag.java
@@ -23,11 +23,11 @@
 package com.semanticcms.dia.taglib;
 
 import static com.aoindustries.encoding.Coercion.zeroIfEmpty;
+import com.aoindustries.encoding.taglib.EncodingBufferedSimpleTag;
 import com.aoindustries.html.servlet.HtmlEE;
 import com.aoindustries.io.buffer.BufferResult;
 import com.aoindustries.io.buffer.BufferWriter;
 import static com.aoindustries.taglib.AttributeUtils.resolveValue;
-import com.aoindustries.taglib.AutoEncodingBufferedTag;
 import com.semanticcms.core.model.ElementContext;
 import com.semanticcms.core.servlet.CaptureLevel;
 import com.semanticcms.core.taglib.ElementTag;
@@ -99,7 +99,7 @@ public class DiaTag extends ElementTag<Dia> {
 			final HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
 			BufferWriter capturedOut;
 			if(captureLevel == CaptureLevel.BODY) {
-				capturedOut = AutoEncodingBufferedTag.newBufferWriter(request);
+				capturedOut = EncodingBufferedSimpleTag.newBufferWriter(request);
 			} else {
 				capturedOut = null;
 			}
