9496dbfd8d4ea7b9864dc864191f4346a88f294d
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:45 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/semanticcms/dia/taglib/book/Maven.java
+++ book/src/main/java/com/semanticcms/dia/taglib/book/Maven.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-dia-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.dia.taglib.book;
 
 import com.aoapps.lang.util.PropertiesUtils;
